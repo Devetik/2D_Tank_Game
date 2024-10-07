5789779401a9b7fbf7ddc3777979eb804bfafb54
Devetik <sebastien@cuttat.dev>
2024-10-08 01:51:17 +0200

    68 Marking Trails

--- Assets/Scripts/Core/Player/PlayerMovement.cs
+++ Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -10,11 +10,28 @@ public class PlayerMovement : NetworkBehaviour
     [SerializeField] private InputReader inputReader;
     [SerializeField] private Transform bodyTransform;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private ParticleSystem dustCloudLeft;
+    [SerializeField] private ParticleSystem dustCloudRight;
     [Header("Serrings")]
     [SerializeField] private float movementSpeed = 4f;
     [SerializeField] private float turningRate = 30f;
+    [SerializeField] private float particleEmmisionValue = 10f;
+
+    private ParticleSystem.EmissionModule emissionModuleLeft;
+    private ParticleSystem.EmissionModule emissionModuleRight;
 
     private Vector2 previousMovementInput;
+
+    private Vector3 previousPos;
+
+    private const float ParticleStopThreshold = 0.005f;
+
+    private void Awake()
+    {
+        emissionModuleLeft = dustCloudLeft.emission;
+
+        emissionModuleRight = dustCloudRight.emission; // A supprimer ou créer un second emissionModule
+    }
 
     public override void OnNetworkSpawn()
     {
@@ -40,6 +57,18 @@ public class PlayerMovement : NetworkBehaviour
 
     private void FixedUpdate()
     {
+        if((transform.position - previousPos).sqrMagnitude > ParticleStopThreshold)
+        {
+            emissionModuleLeft.rateOverTime = particleEmmisionValue;
+            emissionModuleRight.rateOverTime = particleEmmisionValue;
+        }
+        else
+        {
+            emissionModuleLeft.rateOverTime = 0f;
+            emissionModuleRight.rateOverTime = 0f;
+        }
+        previousPos = transform.position;
+
         if(!IsOwner){return;}
 
         rb.velocity = (Vector2)bodyTransform.up * previousMovementInput.y * movementSpeed;
